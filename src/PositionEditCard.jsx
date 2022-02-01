586fd833f1c4a5215cf11f67d97ecedc774e28b8
Greg Martin <gregmartinucb@gmail.com>
2022-01-31 22:11:54 -0800

    Changed layout to card style. Each position will be a card that displays the quickview information, then (eventually) when a card is clicked on it will open an editing card where you will manipulate the information about that postition.

---
+++ src/PositionEditCard.jsx
@@ -0,0 +1,44 @@
+import Form from "react-bootstrap/Form";
+
+let formatter = new Intl.NumberFormat('en-US',{style: 'currency',currency:'USD'});
+
+function PositionEditCard() {
+  return (
+    <div className="card" >
+      <div className="cardElement">
+        <Form.Control type="date" name="Opened"></Form.Control>
+      </div>
+      <div className="cardElement">
+        <input type='text' textAlign={'center'}  placeholder="Ticker"></input>
+      </div>
+     
+      <div className="cardElement">
+        <select type='dropdown' >
+          <option value={'Put'}>Put</option>
+          <option value={'Call'}>Call</option>
+        </select>
+      </div>
+      <div className="cardElement">
+        <select type='dropdown' >
+          <option value={'Sell'}>Sell</option>
+          <option value={'Buy'}>Buy</option>
+        </select>
+      </div>
+
+      <div className="cardElement">
+        <Form.Control type="date" name="Expiration"></Form.Control>
+      </div>
+
+      <div className="cardElement">
+        {/* Current price will use an API to query the current stock price. */}
+        <text type='currency' >{ formatter.format(1) }</text>
+      </div>
+
+      <div className="cardElement">
+        <span>$<input type="text" name="StrikePrice" placeholder="Strike Price"></input></span>
+      </div>
+    </div>
+  );
+} 
+
+export default PositionEditCard;
